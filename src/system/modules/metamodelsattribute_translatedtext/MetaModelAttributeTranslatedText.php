70035b67f3169da0660cc3f7ac51a846f6587b2d
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-05-04 05:07:14 +0200

    Revert commit ea50bd9ab17853483d6509bdba3f75fd60808f83 as the sorting now takes place in AttributeTranslatedReference in the core.

--- src/system/modules/metamodelsattribute_translatedtext/MetaModelAttributeTranslatedText.php
+++ src/system/modules/metamodelsattribute_translatedtext/MetaModelAttributeTranslatedText.php
@@ -51,33 +51,4 @@ class MetaModelAttributeTranslatedText extends MetaModelAttributeTranslatedRefer
 		$arrFieldDef['inputType'] = 'text';
 		return $arrFieldDef;
 	}
-	
-	/**
-	 * Sorts the given array list by field value in the given direction.
-	 *
-	 * This base implementation does a plain SQL sort by native value as defined by MySQL.
-	 *
-	 * @param int[]  $arrIds       A list of Ids from the MetaModel table.
-	 *
-	 * @param string $strDirection The direction for sorting. either 'ASC' or 'DESC', as in plain SQL.
-	 *
-	 * @return int[] The sorted integer array.
-	 */
-	public function sortIds($arrIds, $strDirection)
-	{
-		// Get ids.
-		return Database::getInstance()
-				->prepare(sprintf('SELECT item_id
-					FROM tl_metamodel_translatedtext 
-					WHERE item_id IN(%1$s)
-					AND att_id = ?
-					AND langcode = "%2$s"
-					ORDER BY value' , 
-					implode(", ", $arrIds), // 1
-					$this->getMetaModel()->getActiveLanguage(), // 2
-				    $strDirection // 3
-				))
-				->execute($this->get('id'))
-				->fetchEach('item_id');
-	}
 }
