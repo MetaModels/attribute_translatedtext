5fa8aba2c02ae5ddfad0ed705de0c33102ae9b3b
Sven Baumann <baumann.sv@gmail.com>
2016-12-08 18:25:43 +0100

    for feature deep delete add data provider and child condition configuration

--- contao/dca/tl_metamodel_attribute.php
+++ contao/dca/tl_metamodel_attribute.php
@@ -12,9 +12,51 @@
  * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author      Andreas Isaak <info@andreas-isaak.de>
  * @author      Christopher Boelter <christopher@boelter.eu>
+ * @author      Sven Baumann <baumann.sv@gmail.com>
  * @copyright   The MetaModels team.
  * @license     LGPL.
  * @filesource
  */
 
+/**
+ * Table tl_metamodel_attribute
+ */
+
+/**
+ * Add palette configuration.
+ */
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['metapalettes']['translatedtext extends _complexattribute_'] = array();
+
+/**
+ * Add data provider.
+ */
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['dca_config']['data_provider']['tl_metamodel_translatedtext'] = array
+(
+    'source' => 'tl_metamodel_translatedtext'
+);
+
+/**
+ * Add child condition.
+ */
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['dca_config']['childCondition'][] = array
+(
+    'from'   => 'tl_metamodel_attribute',
+    'to'     => 'tl_metamodel_translatedtext',
+    'setOn'  => array
+    (
+        array
+        (
+            'to_field'   => 'att_id',
+            'from_field' => 'id',
+        ),
+    ),
+    'filter' => array
+    (
+        array
+        (
+            'local'     => 'att_id',
+            'remote'    => 'id',
+            'operation' => '=',
+        ),
+    )
+);
