ebab393336dc5276c3494d9786fb79c7e6cb42d4
David Molineus <david.molineus@netzmacht.de>
2017-12-01 13:53:39 +0100

    Add base bundle structure.

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of MetaModels/attribute_translatedtext.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage AttributeTranslatedText
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\MetaModelsAttributeTranslatedText\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use MetaModels\AttributeTranslatedTextBundle\MetaModelsAttributeTranslatedTextBundle;
+use MetaModels\CoreBundle\MetaModelsCoreBundle;
+
+/**
+ * Plugin for the Contao Manager.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(MetaModelsAttributeTranslatedTextBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class,
+                        MetaModelsCoreBundle::class
+                    ]
+                )
+                ->setReplace(['metamodelsattribute_translatedtext'])
+        ];
+    }
+}
---
+++ src/MetaModelsAttributeTranslatedTextBundle.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of MetaModels/attribute_translatedtext.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage AttributeTranslatedText
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\AttributeTranslatedTextBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * This is the bundle class.
+ */
+class MetaModelsAttributeTranslatedTextBundle extends Bundle
+{
+}
