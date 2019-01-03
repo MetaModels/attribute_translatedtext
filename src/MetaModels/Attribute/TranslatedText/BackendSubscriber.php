3c0a909dd65d3b2077a5c770c53f93393510a939
Mini Model <minimodel@metamodel.me>
2019-01-03 20:25:23 +0100

    Update PHPDoc file headers

--- src/MetaModels/Attribute/TranslatedText/BackendSubscriber.php
+++ src/MetaModels/Attribute/TranslatedText/BackendSubscriber.php
@@ -3,19 +3,18 @@
 /**
  * This file is part of MetaModels/attribute_translatedtext.
  *
- * (c) 2012-2018 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeTranslatedText
+ * @package    MetaModels/attribute_translatedtext
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2018 The MetaModels team.
+ * @copyright  2012-2019 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedtext/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
