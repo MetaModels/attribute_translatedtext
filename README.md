ac1d3aa1e884545857d877b7c952b44cb4a5ce6e
Ingolf Steinhardt <info@e-spin.de>
2022-11-13 11:37:33 +0100

    Fix gactions

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-[![Build Status](https://travis-ci.org/MetaModels/attribute_translatedtext.svg)](https://travis-ci.org/MetaModels/attribute_translatedtext)
+[![Build Status](https://github.com/MetaModels/attribute_translatedtext/actions/workflows/diagnostics.yml/badge.svg)](https://github.com/MetaModels/attribute_translatedtext/actions)
 [![Latest Version tagged](http://img.shields.io/github/tag/MetaModels/attribute_translatedtext.svg)](https://github.com/MetaModels/attribute_translatedtext/tags)
 [![Latest Version on Packagist](http://img.shields.io/packagist/v/MetaModels/attribute_translatedtext.svg)](https://packagist.org/packages/MetaModels/attribute_translatedtext)
 [![Installations via composer per month](http://img.shields.io/packagist/dm/MetaModels/attribute_translatedtext.svg)](https://packagist.org/packages/MetaModels/attribute_translatedtext)
 
 Translated text
-=========================
+===============
 
-The translated text attribute
+The translated text attribute.
--- tests/Attribute/TranslatedTextAttributeTypeFactoryTest.php
+++ tests/Attribute/TranslatedTextAttributeTypeFactoryTest.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of MetaModels/attribute_translatedtext.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,8 @@
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedtext/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -30,6 +31,8 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * Test the attribute factory.
+ *
+ * @covers \MetaModels\AttributeTranslatedTextBundle\Attribute\AttributeTypeFactory
  */
 class TranslatedTextAttributeTypeFactoryTest extends TestCase
 {
